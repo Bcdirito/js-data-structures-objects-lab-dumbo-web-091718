799471a8a69c55442e9617b481e5e8dea272d3be
Brian DiRito <bdirito91@gmail.com>
2019-02-24 21:28:17 -0500

    redid the lab for review

--- index.js
+++ index.js
@@ -1,31 +1,25 @@
 // Write your solution in this file!
-const driver = {
-  driver: "Brian"
+let driver = {}
+
+function updateDriverWithKeyAndValue(driver, key, value){
+  const newObj = {...driver}
+  newObj[key] = value
+
+  return newObj
 }
 
-function updateDriverWithKeyAndValue(driver, key, value) {
-  const newDriver = {...driver};
-
-  newDriver[key] = value;
-
-  return newDriver;
-}
-
-function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
-  driver[key] = value;
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value){
+  driver[key] = value
   return driver
 }
 
-function deleteFromDriverByKey(driver, key) {
-  const newDriver = Object.assign({}, driver);
-
-  delete newDriver[key];
-
-  return newDriver
+function deleteFromDriverByKey(driver, key){
+  const newObj = {...driver}
+  delete newObj[key]
+  return newObj
 }
 
-function destructivelyDeleteFromDriverByKey(driver, key) {
-  delete driver[key];
-
+function destructivelyDeleteFromDriverByKey(driver, key){
+  delete driver[key]
   return driver
 }
